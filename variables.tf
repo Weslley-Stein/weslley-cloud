6f22c6a639316e5368832cffd60551853d692142
Weslley Fernandes <fernandes@Weslleys-MacBook-Pro.local>
2025-07-25 06:11:41 -0300

    fix: wrong condition for region variable

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "region" {
   type = string
   default = "us-east-1"
   validation {
-    condition = var.region == "sa-east-1"
+    condition = !var.region == "sa-east-1"
     error_message = "You are lost in the sauce bruh"
   }
 }
